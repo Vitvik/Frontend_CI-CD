aa84d275395f3dbda51feafa86577bb8df8e883f
Vitvik <Tkachukvitaliy@gmail.com>
2024-06-17 14:06:13 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 /* eslint-disable jsx-a11y/anchor-is-valid */
 //const serverPort = 3001; 
-const serverURL = `http://k8s-vitvik-backendi-edcb51c7c8-53701948.eu-west-1.elb.amazonaws.com`;
+const serverURL = `k8s-vitvik-backendi-edcb51c7c8-1948522189.eu-west-1.elb.amazonaws.com`;
 //`http://localhost:${serverPort}/`;
 
 class CustomerApp extends Component {
